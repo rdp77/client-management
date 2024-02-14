8a744666ec80d5910e20abc7df20a14ca5ecfe35
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2024-02-14 23:21:02 +0700

    :sparkles: add client feature collection

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
 use App\Models\User;
 use Filament\Notifications\Notification;
 use Illuminate\Database\Seeder;
@@ -20,6 +21,8 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.test',
             'password' => Hash::make('admin'),
         ]);
+
+        Client::factory()->count(25)->create();
 
         Notification::make()
             ->title('Welcome to Filament')
